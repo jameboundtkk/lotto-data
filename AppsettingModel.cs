391214819ffdd01e506889c572e2cb2ad1bc1bee
Tu2020 <nakkarin2011@gmail.com>
2020-11-23 15:11:56 +0700

    Initial Code

---
+++ AppsettingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class AppsettingModel
+    {
+        public ConnectionStringModel ConnectionStrings { get; set; }
+    }
+    public class ConnectionStringModel
+    {
+        public string LOTTODB { get; set; }
+    }
+}
---
+++ lottoDBContext.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using lotto_DB.lottoContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lotto_DB
+{
+    public class lottoDBContext : LocaldbMssqllocaldbContext
+    {
+        private readonly AppsettingModel _configuration;
+
+        public lottoDBContext(IOptions<AppsettingModel> configuration)
+        {
+            _configuration = configuration.Value;
+        }
+
+        public lottoDBContext(DbContextOptions<LocaldbMssqllocaldbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.ConnectionStrings.LOTTODB);
+            }
+        }
+    }
+}
